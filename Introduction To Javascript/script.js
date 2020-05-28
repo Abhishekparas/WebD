3ed3bae9504ebd83b2180d28ae201c1fb1ccbe84
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-05-28 20:08:32 +0530

    Add files via upload

---
+++ Introduction To Javascript/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var age = prompt("what is your age?");
+var calc = age*365;
+alert(age + " years is roughly " + calc + " days ");
+console.log("your full name is " + firstName + lastName);
+console.log("you are " + age + " years old");
