d2aa0b6855f5315f0de3820260f02234fbcb51c2
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-08-18 17:54:32 +0530

    Add files via upload

---
+++ Modern JS/math.js
@@ -0,0 +1,55 @@
+let val;
+val = Math.PI;
+console.log(val);
+
+val = Math.E
+console.log(val);
+
+val = Math.round(2.4);
+console.log(val);
+
+val = Math.ceil(2.4);
+
+console.log(val);
+
+val = Math.floor(2.8);
+console.log(val);
+
+val = Math.sqrt(64);
+console.log(val);
+
+val = Math.pow(2,3);        
+console.log(val);
+
+val = Math.abs(-2);
+console.log(val);
+
+val = Math.min(54,65,23,84);
+console.log(val);
+
+val = Math.max(54,65,23,84);
+console.log(val);
+
+val = Math.random();            //generates float random numbers
+console.log(val);
+
+val = Math.random()*20;         //generates float numbers between 0 and 19
+console.log(val);
+
+val = Math.random()*20 + 1;     //generates float numbers between 1 and 20
+console.log(val);
+
+val = Math.floor(Math.random()*20 + 1);
+console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
