ad59de156a3bb8dadb5f22435bcd513e17bbbb37
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-08-18 17:49:06 +0530

    Add files via upload

---
+++ Modern JS/app.js
@@ -0,0 +1,75 @@
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+
+console.error("this is an error");
+console.clear();
+console.warn("this is a warning");
+
+console.time('hello');
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+    console.log("hello world!!!");
+console.timeEnd('hello');
+
+var name = "john doe";
+console.log(name);
+
+name = "steve smith";
+console.log(name);
+
+var greeting;
+console.log(greeting);
+
+greeting = 'hello';
+console.log(greeting);
+
+
+let name1 = "sarah";
+console.log(name1);
+
+const name2 = "harry";
+console.log(name2);
+
+//name2 = "potter"; //error bcz it is a const
+
+const person = {
+    name: "mike",
+    age: 30
+}
+
+console.table(person);
+
+person.age = 35;
+console.table(person);
+
+const arr = [1,2,3,4,5];
+console.log(arr);
+
+arr.push(6);
+console.log(arr);
+
+const today = new Date();
+console.log(today);
+
+
+
+
+
+
+
+
+
+
+
+
