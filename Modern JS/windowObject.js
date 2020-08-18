2f7d0bb84c3964b4ac48a74ee075f8d3a6e624be
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-08-18 17:57:00 +0530

    Add files via upload

---
+++ Modern JS/windowObject.js
@@ -0,0 +1,31 @@
+let val;
+
+val = window.outerHeight;
+val = window.outerWidth;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+val = window.scrollY;
+val = window.scrollX;
+
+val = window.location;
+
+val = window.location.port;
+val = window.location.hostname;
+val = window.location.href;
+val = window.location.search;
+//window.location.search = 'https://www.google.com';
+
+//window.location.reload();
+
+//window.history.go(-1);
+val = window.history.length;
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+console.log(val);
